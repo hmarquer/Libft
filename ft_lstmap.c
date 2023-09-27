3474812137cc2e37b06ff7ac4eca529e55c8f4c0
Hugo Marquerie Martín <hugomarquerie@gmail.com>
2023-09-28 00:15:57 +0200

    bonus done

---
+++ ft_lstmap.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*ret;
+	t_list	*tmp;
+
+	ret = 0;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&ret, del);
+			return (0);
+		}
+		ft_lstadd_back(&ret, tmp);
+		lst = lst->next;
+	}
+	return (ret);
+}
